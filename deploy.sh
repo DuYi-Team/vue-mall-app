e43689da35968621bbccb6996ee3d16300bb1b13
x-cold <lxstart@outlook.com>
2020-05-03 10:46:31 +0800

    feat: node-sass use cnpm

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:DuYi-Team/vue-mall-app.git master:gh-pages
 
 cd -
